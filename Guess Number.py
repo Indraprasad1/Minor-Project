211979ca08342fe057e7ad50f81c5bb860c28643
Indra <indarchaudhary0@gmail.com>
2025-03-31 23:03:17 +0530

    initial commit

---
+++ Guess Number.py
@@ -0,0 +1,38 @@
+import random
+
+name = input("What is Your name? ")
+print("Good Luck! ", name)
+
+words = ['rainbow', 'computer', 'science', 'programming', 'python', 'mathematics', 'player']
+word = random.choice(words)
+
+print("Guess The characters")
+
+guesses = ''
+turns = 12
+
+while turns > 0:
+    failed = 0
+    for char in word:
+        if char in guesses:
+            print(char, end=" ")
+        else:
+            print("_")
+            failed += 1
+            
+    if failed == 0:
+        print("You Win")
+        print("The word is: ", word)
+        break
+    
+    print()
+    guess = input("guess a character:")
+    guesses += guess
+
+    if guess not in word:
+        turns -= 1
+        print("Wrong")
+        print("You have", + turns, 'more guesses')
+
+        if turns == 0:
+            print("You loose")
